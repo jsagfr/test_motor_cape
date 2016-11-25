d43265f9576d7994e9e7eb9daa5add35053c9c7f
Jérôme Guéry <jerome.guery@gmail.com>
2016-11-25 09:36:22 +0000

    write command bug correction

--- test_app/src/main.cpp
+++ test_app/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include  <iomanip>
 #include <thread>
 #include <chrono>
 #include "MotorBridgeCape.h"
@@ -13,36 +14,36 @@ int main (int argc, char *argv[])
   cape.setDCMotorFrequency(1000);
 
   cape.setChannel1(ChannelType::DcDc);
-  cape.setChannel2(ChannelType::DcDc);
+  // cape.setChannel2(ChannelType::DcDc);
 
 
   auto channelDcDc1 = static_cast<ChannelDcDc*>(cape.getChannel1());
-  auto channelDcDc2 = static_cast<ChannelDcDc*>(cape.getChannel2());
+  // auto channelDcDc2 = static_cast<ChannelDcDc*>(cape.getChannel2());
 
   auto motor1A = dynamic_cast<DCMotor*>(channelDcDc1->getMotorA());
-  auto motor1B = dynamic_cast<DCMotor*>(channelDcDc1->getMotorB());
-  auto motor2A = dynamic_cast<DCMotor*>(channelDcDc2->getMotorA());
-  auto motor2B = dynamic_cast<DCMotor*>(channelDcDc2->getMotorB());
-  if (!motor1A || !motor1B || !motor2A || !motor2B)
-    {
-      return EXIT_FAILURE;
-    }
+  // auto motor1B = dynamic_cast<DCMotor*>(channelDcDc1->getMotorB());
+  // auto motor2A = dynamic_cast<DCMotor*>(channelDcDc2->getMotorA());
+  // auto motor2B = dynamic_cast<DCMotor*>(channelDcDc2->getMotorB());
+  // if (!motor1A || !motor1B || !motor2A || !motor2B)
+  //   {
+  //     return EXIT_FAILURE;
+  //   }
   
-  std::cout << "power to 0.25" << std::endl;
+  std::cout << "power to 0.5" << std::endl;
     
   motor1A->power(0.5);
-  motor2A->power(0.5);
-  motor1B->power(0.5);
-  motor2B->power(0.5);
+  // motor2A->power(0.5);
+  // motor1B->power(0.5);
+  // motor2B->power(0.5);
 
   std::this_thread::sleep_for(10s);
 
   std::cout << "stop" << std::endl;
 
   motor1A->stop();
-  motor2A->stop();
-  motor1B->stop();
-  motor2B->stop();
+  // motor2A->stop();
+  // motor1B->stop();
+  // motor2B->stop();
 
   return 0;
 }
